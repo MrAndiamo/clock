6663898902e67c00298af681c80cbdb27ea06d29
Tim van den Dries <tim.vandendries@brightfish.com>
2023-04-01 03:07:36 +0200

    debug

--- src/Clock.php
+++ src/Clock.php
@@ -116,11 +116,11 @@ class Clock {
                         var second = now.getSeconds();
                         hour = hour%12;
                         hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
-                        drawHand(ctx, hour, radius*0.5, radius*0.07, ' . $this->clockHandHoursColor . ');
+                        drawHand(ctx, hour, radius*0.5, radius*0.07, "' . $this->clockHandHoursColor . '");
                         minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
-                        drawHand(ctx, minute, radius*0.8, radius*0.07, ' . $this->clockHandMinutesColor . ');
+                        drawHand(ctx, minute, radius*0.8, radius*0.07, "' . $this->clockHandMinutesColor . '");
                         second = (second*Math.PI/30);
-                        drawHand(ctx, second, radius*0.9, radius*0.02, ' . $this->clockHandSecondsColor . ');
+                        drawHand(ctx, second, radius*0.9, radius*0.02, "' . $this->clockHandSecondsColor . '");
                     }';
 
         $drawHand = 'function drawHand(ctx, pos, length, width, color) {
