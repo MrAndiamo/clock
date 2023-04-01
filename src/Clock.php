b78adecc691b4a655dca4131bbcd2302480bf613
Tim van den Dries <tim.vandendries@brightfish.com>
2023-04-01 02:08:07 +0200

    made canvas clock

--- src/Clock.php
+++ src/Clock.php
@@ -29,109 +29,97 @@ class Clock {
 
     public function showClock() : string {
 
-        $css = self::getCSS();
-        $javascript = self::getJavascript();
-        $html = self::getHTML();
-        return $css . $html . $javascript;
-    }
 
 
-    private function getHTML() : string {
-        return '<div class=\'TD_clock\'>
-                    <div id=\'TD_clock_hours\'></div>
-                    <div id=\'TD_clock_minutes\'></div>
-                    <div id=\'TD_clock_seconds\'></div>
-                </div>';
-    }
-
-    private function getCSS() : string {
-
-        $clockHandHeight = ($this->clockSize / 2) - 10;
-        $clockHandHeightHours = $clockHandHeight * 0.7;
-        $clockHandHeightHoursTop = $clockHandHeight - $clockHandHeightHours + 10;
-
-        $clockBorder = $this->clockBorderWidth == 0 ? 'none' : $this->clockBorderWidth . 'px solid ' . $this->clockBorderColor;
-
-        $TD_clock_CSS = '.TD_clock {
-                            position: relative;
-                            height: ' . $this->clockSize . 'px;
-                            width: ' . $this->clockSize .'px;
-                            background-color: ' . $this->clockBackgroundColor . ';
-                            border: ' . $clockBorder . '
-                            border-radius: 50%;
-                        }';
-
-        $TD_clock_div_CSS = '.TD_clock div {
-                                position: absolute;
-                                top: 10px;
-                                left: 50%;
-                                height: ' . $clockHandHeight . 'px;
-                                width: ' . $this->clockHandWidth . 'px;
-                                transform: translateX(50%);
-                                transform: rotate(0deg);
-                                transform-origin: bottom center;
-                            }';
-
-        $TD_clock_hours = '#TD_clock_hours {
-                                top: ' . $clockHandHeightHoursTop . 'px;
-                                background-color: ' . $this->clockHandMinutesColor . ';
-                                height: ' . $clockHandHeightHours . 'px;
-                                
-                            }';
-
-        $TD_clock_minutes = '#TD_clock_minutes {
-                                background-color: ' . $this->clockHandMinutesColor . ';
-                            }';
-
-        $TD_clock_seconds = '#TD_clock_seconds {
-                                background-color: ' . $this->clockHandSecondsColor . ';
-                            }';
-
-        return '<style>' .
-            $TD_clock_CSS .
-            $TD_clock_div_CSS .
-            $TD_clock_hours .
-            $TD_clock_minutes .
-            $TD_clock_seconds .
-            '</style>';
-
-    }
+        $canvasHTML = '<canvas id="canvas" width="300" height="300"></canvas>';
 
 
-    private static function getJavascript() {
-        $javascript = 'const updateInMS = 1000;
-            
-                    const clock = document.getElementsByClassName(\'clock\')[0];
-                    const htmHours = document.getElementById(\'TD_clock_hours\');
-                    const htmMinutes = document.getElementById(\'TD_clock_minutes\');
-                    const htmSeconds = document.getElementById(\'TD_clock_seconds\');
-            
-                    startTimer();
-            
-                    function startTimer() {
-                        tick();
-                    }
-            
-                    function tick() {
-                        setTimeout(function() {
-                            const now = new Date();
-                            const hours = now.getHours();
-                            const minutes = now.getMinutes();
-                            const seconds = now.getSeconds();
-            
-                            const secondsInDegrees = (360 * seconds) / 60;
-                            const minutesInDegrees = (360 * minutes) / 60;
-                            const hoursInDegrees = (360 * hours) / 12;
-            
-                            htmHours.style.transform = \'rotate(\' + hoursInDegrees + \'deg)\';
-                            htmMinutes.style.transform = \'rotate(\' + minutesInDegrees + \'deg)\';
-                            htmSeconds.style.transform = \'rotate(\' + secondsInDegrees + \'deg)\';
-            
-                            tick();
-                        }, updateInMS);
-                    }';
+        $canvas = 'var canvas = document.getElementById("canvas");
+            var ctx = canvas.getContext("2d");
+            var radius = canvas.height / 2;
+            ctx.translate(radius, radius);
+            radius = radius * 0.90;';
 
-        return '<script>' . $javascript . '</script>';
+        $interval = 'setInterval(drawClock, 1000);';
+
+
+        $drawClock = 'function drawClock() {
+                drawFace(ctx, radius);
+                drawNumbers(ctx, radius);
+                drawTime(ctx, radius);
+            }';
+
+        $drawFace = 'function drawFace(ctx, radius) { 
+                var grad;
+                ctx.beginPath();
+                ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+                ctx.fillStyle = "white";
+                ctx.fill();
+                grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
+                grad.addColorStop(0, "#333");
+                grad.addColorStop(0.5, "white");
+                grad.addColorStop(1, "#333");
+                ctx.strokeStyle = grad;
+                ctx.lineWidth = radius*0.1;
+                ctx.stroke();
+            }';
+
+
+        $drawNumbers = 'function drawNumbers(ctx, radius) {
+                  // draw middle circle and set color
+                  ctx.beginPath();
+                  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+                  ctx.fillStyle = "#CC0000";
+                  ctx.fill();
+                      
+                  // draw numbers
+                  var ang;
+                  var num;
+                  ctx.font = radius * 0.15 + "px arial";
+                  ctx.textBaseline = "middle";
+                  ctx.textAlign = "center";
+                  ctx.fillStyle = "#000000";
+                  for(num = 1; num < 13; num++){
+                      ang = num * Math.PI / 6;
+                      ctx.rotate(ang);
+                      ctx.translate(0, -radius * 0.85);
+                      ctx.rotate(-ang);
+                      ctx.fillText(num.toString(), 0, 0);
+                      ctx.rotate(ang);
+                      ctx.translate(0, radius * 0.85);
+                      ctx.rotate(-ang);
+                  }
+            }';
+
+
+        $drawTime = 'function drawTime(ctx, radius){
+                var now = new Date();
+                var hour = now.getHours();
+                var minute = now.getMinutes();
+                var second = now.getSeconds();
+                hour = hour%12;
+                hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+                drawHand(ctx, hour, radius*0.5, radius*0.07);
+                minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
+                drawHand(ctx, minute, radius*0.8, radius*0.07);
+                second = (second*Math.PI/30);
+                drawHand(ctx, second, radius*0.9, radius*0.02);
+            }';
+
+        $drawHand = 'function drawHand(ctx, pos, length, width) {
+                ctx.fillStyle = "black";
+                ctx.beginPath();
+                ctx.lineWidth = width;
+                ctx.lineCap = "round";
+                ctx.moveTo(0,0);
+                ctx.rotate(pos);
+                ctx.lineTo(0, -length);
+                ctx.stroke();
+                ctx.rotate(-pos);
+            }';
+        $javascript = $canvas . $interval . $drawClock . $drawFace . $drawNumbers . $drawTime . $drawHand;
+        return $canvasHTML . '<script>' . $javascript . '</script>';
+        
     }
 
 }
